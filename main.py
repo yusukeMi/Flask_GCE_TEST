3d286a7450b49199ef59fa00b679ec3f5706580e
yusukeMi <libertad.dc2@gmail.com>
2023-12-19 12:28:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
-import flask
+from flask import Flask, request
+
 app = Flask(__name__)
-# Index route
-@app.route('/')
-def index():
-    return render_template('index.html')
-    # We are telling flask to look for a file named index in the templates folder
-    #then render it to the user interface. Note that by default flask looks for templates in a folder named templates.
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route("/")
+def root():
+    return "helloworld"
+
+if __name__ == "__main__":
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host="127.0.0.1", port=8080, debug=True)
